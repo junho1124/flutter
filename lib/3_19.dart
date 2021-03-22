f6c17f1f1724415054b93ce2141275b505d743c5
JINO <wnsghqwe@gmail.com>
2021-03-22 10:28:34 +0900

    'up'

---
+++ lib/3_19.dart
@@ -0,0 +1,19 @@
+void main() {
+  //수식
+  print('hello world');
+  print('hahaha');
+  print(2.5 / 2); // 1.35
+  print(2.5 * 2);
+  print(2.5 + 2);
+  print(2.5 % 2);
+  print('2.5 - 2 = ${2.5 - 2}'); //글 사이에 수식을 넣을 때는 'text ${수식}'
+  print(2.5 / 2);
+
+  //변수
+  int x;  //integer 정수 변수 선언(declare)
+  x = 10; // 번수 x에 10을 대입(assign)
+
+  int x = 10; // 선언과 동시에 대입(초기화)
+  x = 20; //변수 재 할당은 곧 안될 예정
+  print('안녕하세요. 저는 ${x - 5 * 2}살 입니다.'); // 수식을 대입 가능
+}
---
+++ lib/3_22.dart
@@ -0,0 +1,40 @@
+void main() {
+//변수의 이름은 영어, _, 숫자 등으로 만든다.
+  int a = 10; // 잘못된 이름
+  //잘 된 이름(camel case)
+  int myAge = 10; // 나이를 camel case로 표시
+  //int myAge = 10; / 같은이름으로 다시 변수를 쓸수 없음 / 대소문자 구분할것
+  int brotherAge = 20;
+  //snake case (뱀형)
+  int my_age = 10;
+  int brother_age = 20;
+  //이미 사용된 변수는 사용 x
+
+  //[숫자형]
+  // int d = 5.5; 는 안됨
+  double d = 5.5; // 실수(소수점만)
+
+//  [문자영]
+  String name = '홍길동';
+
+//  [진워 (참, 거짓)]
+  bool isMarried = true;
+  isMarried = false;
+
+//  목록
+
+//숫자 목록
+  List<int> ageList = [10, 20, 30];
+  var ageList2 = [10, 20, 30]; // 모든걸 다 가능(가급적 정의하는게 좋으나 모르겠을 때 var를 쓰면 알아서 해줌)
+// 문자열 목록
+  List nameList = ['홍길동', '한성복'];
+  var nameList2 = ['홍길동', '한길동']
+//  null(값이 없다)
+  int g; // null (0은 아님)
+  // int g = 0 // 0
+  g = 10; // null이 아니고 10
+
+  String nameList; // null
+
+
+}
