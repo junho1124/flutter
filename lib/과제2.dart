c4e359e78522a4cf32ee77cae2097520baedc0f3
JINO <wnsghqwe@gmail.com>
2021-03-22 11:55:49 +0900

    '최종수정'

--- lib/과제2.dart
+++ lib/과제2.dart
@@ -1,7 +1,12 @@
 void main() {
   bool sample = true;
   bool unsample = false;
-  double x = 3.14;
+  int x = 10;
+    if(sample||x == 10){
+      print("true");
+    } else{print("flase");
+    }
+  double z = 3.14;
   int y = 314159265853979;
   String name = '항구를 공격! 적에게 15포인트의 데미지를 주었다';
-}
+    }
