867870b5a16f9d5f433a5622f31463c839c6fafb
choiJunHo <wnsghqwe@gmail.com>
2021-03-30 14:08:11 +0900

    DOO6 solve

---
+++ lib/3_30/exam10.dart
@@ -0,0 +1,25 @@
+import 'dart:math';
+
+void main() {
+  Math exam = Math();
+  print(exam.range(1, 'km'));
+  print(exam.range(1, 'm'));
+  print(exam.range(1, 'cm'));
+}
+
+class Math {
+  String range(int n, String b) {
+    String result = '';
+    int km = n * 1000;
+    int m = n * 100;
+    int cm = n * 10;
+    if (b == 'km') {
+      result = '$km';
+    } else if (b == 'm') {
+      result = '$m';
+    } else if (b == 'cm') {
+      result = '$cm';
+    }
+    return result;
+  }
+}
--- lib/3_30/exam8.dart
+++ lib/3_30/exam8.dart
@@ -3,17 +3,15 @@ import 'dart:math';
 void main() {
   List<String> inputs = ['Java', 'Gino', "Junho"];
   Exam exam = Exam();
-  print( exam.hello(inputs));
-
+  print(exam.hello(inputs));
 }
 
 class Exam {
   String hello(List<String> list) {
     String result = '';
     for (int i = 0; i < list.length - 1; i++) {
-
       result += '${list.elementAt(i)} ,';
     }
-    return 'hello '+ result + '${list.elementAt(list.length - 1)}.';
+    return 'hello ' + result + '${list.elementAt(list.length - 1)}.';
   }
 }
