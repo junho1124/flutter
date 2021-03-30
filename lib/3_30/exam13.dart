b597a4eb446d791c3e3a806f94f88585bfbd3538
choiJunHo <wnsghqwe@gmail.com>
2021-03-30 14:52:13 +0900

    DO13 solve

---
+++ lib/3_30/exam13.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+
+void main() {
+  Math exam = Math();
+  print(exam.division(130, 3));
+  print(exam.division(10, 3));
+  print(exam.division(12, 14));
+
+}
+
+class Math {
+  String division(int m, int n) {
+    int rem = m % n;
+    int div = m ~/ n;
+    String result = '$div $rem';
+
+    return result;
+  }
+}
