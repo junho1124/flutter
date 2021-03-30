de698e257bdd9b4979e9a56ae8da1299471a145e
choiJunHo <wnsghqwe@gmail.com>
2021-03-30 13:28:15 +0900

    DOO5 solve

---
+++ lib/3_30/exam9.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+
+void main() {
+  Math exam = Math();
+  print(exam.numbers(3, 3));
+  print(exam.numbers(5, 10));
+  print(exam.numbers(1, 3));
+}
+
+class Math {
+  String numbers(int a, int b) {
+    String result = '';
+    for (int i = 0; i < 10; i++) {
+      int funtion = a + b * i;
+      result += '$funtion ';
+    }
+    return result;
+  }
+}
