9b9a1147e42b4f0ec87689072eb566282da9c03d
choiJunHo <wnsghqwe@gmail.com>
2021-03-30 11:44:42 +0900

    DOO4 solve

---
+++ lib/exam8.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+
+void main() {
+  List<String> inputs = ['Java', 'Gino', "Junho"];
+  Exam exam = Exam();
+  print( exam.hello(inputs));
+
+}
+
+class Exam {
+  String hello(List<String> list) {
+    String result = '';
+    for (int i = 0; i < list.length - 1; i++) {
+
+      result += '${list.elementAt(i)} ,';
+    }
+    return 'hello '+ result + '${list.elementAt(list.length - 1)}.';
+  }
+}
