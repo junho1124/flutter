2f35598ea885f8f2ec72ac7e42e700b4e99b931c
choiJunHo <wnsghqwe@gmail.com>
2021-03-31 10:00:08 +0900

    D006 solve EX again

--- lib/3_30/exam10.dart
+++ lib/3_30/exam10.dart
@@ -3,22 +3,22 @@ import 'dart:math';
 void main() {
   Math exam = Math();
   print(exam.range(1, 'km'));
-  print(exam.range(1, 'm'));
+  print(exam.range(5, 'm'));
   print(exam.range(1, 'cm'));
 }
 
 class Math {
   String range(int n, String b) {
     String result = '';
-    int km = n * 1000;
-    int m = n * 100;
-    int cm = n * 10;
+    int km = 1000;
+    int m =  100;
+    int cm = 10;
     if (b == 'km') {
-      result = '${km * m * cm}';
+      result = '${ n * km * m * cm}';
     } else if (b == 'm') {
-      result = '${m * cm}';
+      result = '${n * m * cm}';
     } else if (b == 'cm') {
-      result = '$cm';
+      result = '${n * cm}';
     }
     return result;
   }
