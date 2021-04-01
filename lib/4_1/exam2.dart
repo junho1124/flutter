e3aad99be081f0db3052c186cbd3e6cf9b0fa13f
choiJunHo <wnsghqwe@gmail.com>
2021-04-01 11:20:17 +0900

    D023 solve agian

--- lib/4_1/exam2.dart
+++ lib/4_1/exam2.dart
@@ -1,29 +1,36 @@
+// import 'dart:io';
+//
+// void main() {
+//   String s = stdin.readLineSync()!;
+//
+//   int result = 0;
+//   for (int i = 0; i < s.length; i++) {
+//
+//     if (s.substring(i, i + 1) == 'A') {
+//       result = result + 1;
+//     }
+//   } return print(result);
+// }
+
 import 'dart:io';
 
 void main() {
+  CountString countString = CountString();
   String s = stdin.readLineSync()!;
-
-  int result = 0;
-  for (int i = 0; i < s.length; i++) {
-
-    if (s.substring(i, i + 1) == 'A') {
-      result = result + 1;
-    }
-  } return print(result);
+  print(countString.solution(s));
 }
+class CountString {
 
-// class CountString {
-//
-//   int solution() {
-//     String s = '';
-//
-//     int result = 0;
-//     for (int i = 0; i < s.length; i++) {
-//
-//       if (s.substring(i, i + 1) == 'A') {
-//         result = result + 1;
-//       }
-//     } return result;
-//   }
-//
-// }
+  int solution(String s) {
+    String text = s;
+
+    int result = 0;
+    for (int i = 0; i < text.length; i++) {
+
+      if (text.substring(i, i + 1) == 'A') {
+        result = result + 1;
+      }
+    } return result;
+  }
+
+}
