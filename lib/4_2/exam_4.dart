46d26f48bf4f7bc4d3d60817f1f8aac2c1c3f2cc
choiJunHo <wnsghqwe@gmail.com>
2021-04-02 10:29:29 +0900

    뮨제 4

---
+++ lib/4_2/exam_4.dart
@@ -0,0 +1,21 @@
+void main() {
+  Math exam = Math();
+  print(exam.numbers('5555'));
+  print(exam.numbers('3335'));
+}
+class Math {
+  String numbers(String n) {
+    String result = n;
+    for (int i = 0; i < n.length - 1; i++) {
+      if (getCharacter(n, i) !=
+          getCharacter(n, i + 1)) {
+        result = 'No';
+        break;
+      }
+    }
+    return result;
+  }
+  String getCharacter(String str, int i) {
+    return str.substring(i, i + 1);
+  }
+}
